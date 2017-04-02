282de9a196539c59af66c24a4fb41c9d9fccac5f
Nick Kotzalas <nkotzalas@gmail.com>
2017-04-02 12:25:25 +0300

    Fixed error in example

--- examples/lsm303accelerometer/LSM303Accelerometer.ino
+++ examples/lsm303accelerometer/LSM303Accelerometer.ino
@@ -5,20 +5,20 @@
 const bool debug = true;
 
 // Create the instance of the LSM303Accelerometer
-LSM303Accelerometer LSM303Accelerometer = LSM303Accelerometer(debug);
+LSM303Accelerometer lsm303Accelerometer = LSM303Accelerometer(debug);
 
 void setup() {
   Serial.begin(9600);
 
   // Initialize the accelerometer sensor
-  LSM303Accelerometer.setup();
+  lsm303Accelerometer.setup();
 }
 
 /*
  * Calculates the orientation every 1 second and prints it to the Serial monitor
  */
 void loop() {
-  float orientation = LSM303Accelerometer.calculateOrientation();
+  float orientation = lsm303Accelerometer.calculateOrientation();
   Serial.print("Orientation = ");
   Serial.println(orientation);
   
